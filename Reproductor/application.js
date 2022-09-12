9c02d969959b856e84bf177302e5359bad122ad7
Yarissa Debroy <yarissadebroy@gmail.com>
2022-09-11 19:00:19 -0600

    Actualización del reproductor

--- Reproductor/application.js
+++ Reproductor/application.js
@@ -1,23 +1,84 @@
+
 function iniciar(){
-    Maximo=600;
-    Medio=document.getElementById('medio');
-    reproducir= document.getElementById('reproducir');
-    barra= document.getElementById('barra');
-    progreso= document.getElementById('progreso');
-    
+    maximo = 600;
+    video = document.getElementById('medio');
+    reproducir = document.getElementById('reproducir');
+    barra = document.getElementById('barra');
+    progreso = document.getElementById('progreso');
+    retroceder= document.getElementById('retroceder');
+    avanzar= document.getElementById('adelantar');
+    expandirpantalla= document.getElementById('fullscreen');
+    tiempo=document.getElementById('tiempo');
+
     reproducir.addEventListener('click', presionar, false);
     barra.addEventListener('click', mover, false);
-    }
-    
-    function presionar(){
-    if(medio.paused && medio.ended){
-    Medio.pause();
-    Reproducer.innerHTML = 'Reproducir';
-    Window.clearInterval(bucle);
+    retroceder.addEventListener('click', rewind,false);
+    avanzar.addEventListener('click', forward, false);
+    expandirpantalla.addEventListener('click', expandir, false);
+    tiempo.addEventListener('timeupdate', time, true);
+
+
+}
+
+function time(e){
+document.getElementById("").innerHTML;
+}
+
+function rewind(e){
+    video.currentTime=video.currentTime -((video.duration/100)*5);
+
+}
+
+function forward(e){
+    video.currentTime=video.currentTime +((video.duration/100)*5);
+}
+
+function expandir(e){
+e.preventDefault();
+video.requestFullscreen();
+}
+
+
+function presionar(){
+    if(!video.paused && !video.ended){
+        video.pause();
+        document.querySelector(".fa-play").style.display="block";
+        document.querySelector(".fa-pause").style.display="none";
+        window.clearInterval(bucle);
+
     }else{
-    Medio.play();
-    Reproducer.innerHTML= 'Pausar';
-    Bucle= setInterval(estado, 1000);
+        video.play();
+        document.querySelector(".fa-play").style.display="none";
+        document.querySelector(".fa-pause").style.display="block";
+        
+        bucle = setInterval(estado, 1000);
     }
+}
+
+function mover(e){
+    if(!video.paused && !video.ended){
+        var ratonX= e.pageX - barra.offsetLeft;
+        var nuevoTiempo = ratonX * video.duration/maximo;
+        video.currentTime= nuevoTiempo;
+        progreso.style.width= ratonX + 'px'
+        
+    }else{
+       video.play(); 
     }
-    
+
+    }
+
+
+
+function estado() {
+    if(!video.ended){
+        var total = parseInt(video.currentTime*600/ video.duration);
+        progreso.style.width = total + 'px';
+    }else{
+        progreso.style.width = '0px';
+        reproducir.innerHTML = "reproducir";
+        window.clearInterval(bucle);
+    }
+}
+
+window.addEventListener('load', iniciar, false);
